f99dd3723b3d83239f15378975b61e1c4c0acecf
Paul Green <paulgreen5555@gmail.com>
2018-09-23 11:07:28 +0300

    First version.

---
+++ App_Code/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+public class Calculator
+{
+    public static string Calculate(string a, string b, string c)
+    {
+        string result = "";
+        double number;
+        if (double.TryParse(a, out number) && double.TryParse(b, out number) && double.TryParse(c, out number))
+        {
+            double A = Convert.ToDouble(a);
+            double B = Convert.ToDouble(b);
+            double C = Convert.ToDouble(c);
+
+            double D = (B * B) - (4 * A * C);
+
+            double x1;
+            double x2;
+            string bSign = B > 0 ? "+" : "-";
+            string cSign = C > 0 ? "+" : "-";
+
+            if (D >= 0)
+            {
+                x1 = ((-1) * B + Math.Sqrt(D)) / 2 / A;
+                x2 = ((-1) * B - Math.Sqrt(D)) / 2 / A;
+
+                result = string.Format("Square equation: {0}x&sup2;{1}{2}x{3}{4}=0 <br> First value: {5} <br> Second value: {6} ", A, bSign, B, cSign, C, x1, x2);
+            }
+            else
+            {
+                // TODO complex numbers
+                result = "D < 0";
+            }
+        }
+        else
+        {
+            result = "Wrong parametrs";
+        }
+        return result;
+    }
+}
---
+++ App_Code/MyHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel.Channels;
+using System.Web;
+
+public class MyHandler : IHttpHandler
+{
+    public void ProcessRequest(HttpContext context)
+    {
+
+        HttpResponse resp = context.Response;
+        HttpRequest req = context.Request;
+
+        string basePath = HttpRuntime.AppDomainAppPath;
+
+        string body = File.ReadAllText(basePath + "body.txt");
+
+        string[] param = new string[0]; 
+
+        string result = req.Url.LocalPath;
+
+        param = result.Split('/');
+
+        if (param.Length == 4)
+        {
+            string a = param[1];
+            string b = param[2];
+            string c = param[3];
+
+            result = Calculator.Calculate(a, b, c);
+        }
+        else
+        {
+            result = "Wrong parametrs";
+        }
+
+        string output = String.Format(body, result);
+
+        resp.Write(output);
+    }
+
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+}
